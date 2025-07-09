3559c858db4eca0e78ed1aeba2c983715b9ba388
Manikandan <smanikandan@ieng6-202.ucsd.edu>
2025-07-09 12:58:21 -0700

    Changes Made

--- utf8analyzer.c
+++ utf8analyzer.c
@@ -9,6 +9,13 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "Usage: utf8analyzer \"<UTF-8 encoded string>\"\n");
         return 1;
     }
-
+}
     // implement the UTF-8 analyzer here
+    void is_ASCII(char word[]) {
+	    for(int i = 0; i != 0; i++) {
+		    if(word[i] >= 0 && word[i] <= 127) {
+			    printf("%c", true);
+		    }
+	    }
+	    printf("%c", false);
 }
